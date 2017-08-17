a75d6eed56524eaa9614731eb6b50405ff2c6bf3
Leonardo Melo <leoomelo@gmail.com>
2017-08-16 21:16:50 -0300

    incluindo lib expect. Refatorando testes existentes usando expect e adicionando novo método setName

--- section6/node-tests/utils/utils.js
+++ section6/node-tests/utils/utils.js
@@ -5,3 +5,11 @@ module.exports.add = (a, b) => {
 module.exports.square = (a) => {
   return a * a;
 }
+
+module.exports.setName = (user, fullname) => {
+	var names = fullname.split(' ');
+	user.firstName = names[0];
+	user.lastName = names[1];
+
+	return user;
+}
--- section6/node-tests/utils/utils.test.js
+++ section6/node-tests/utils/utils.test.js
@@ -1,17 +1,21 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   var res = utils.square(9);
 
-  if (res != 81) {
-    throw new Error(`Expected 81, but got ${res}`);
-  }
+  expect(res).toBe(81).toBeA('number');
+})
+
+it('should set firstName and lastName', () => {
+	var user = {location: "Recife", age: "31"}
+	var res = utils.setName(user, "Leonardo Melo");
+
+	expect(res).toInclude({firstName: "Leonardo", lastName: "Melo"});
 })
