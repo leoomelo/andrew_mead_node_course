a240647c0bd8d29fc85e6ad00ff1bf699d04e25e
Leonardo Melo <leoomelo@gmail.com>
2017-08-17 08:38:34 -0300

    testes unitários de métodos assíncronos

--- section6/node-tests/utils/utils.js
+++ section6/node-tests/utils/utils.js
@@ -2,8 +2,20 @@ module.exports.add = (a, b) => {
   return a + b;
 }
 
+module.exports.asyncAdd = (a, b, callback) => {
+    setTimeout( () => {
+    	callback(a + b);
+    }, 1000);
+}
+
 module.exports.square = (a) => {
   return a * a;
+}
+
+module.exports.asyncSquare = (x, callback) => {
+	setTimeout( () => {
+		callback(x * x);
+	}, 1000);
 }
 
 module.exports.setName = (user, fullname) => {
--- section6/node-tests/utils/utils.test.js
+++ section6/node-tests/utils/utils.test.js
@@ -7,15 +7,30 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+	utils.asyncAdd(3, 4, (num) => {
+		expect(num).toBe(7).toBeA('number');
+		done();
+	});
+	
+});
+
 it('should square a number', () => {
   var res = utils.square(9);
 
   expect(res).toBe(81).toBeA('number');
-})
+});
+
+it('should async square a number', (done) => {
+	utils.asyncSquare(6, (num) => {
+		expect(num).toBe(36).toBeA('number');
+		done();
+	});
+});
 
 it('should set firstName and lastName', () => {
 	var user = {location: "Recife", age: "31"}
 	var res = utils.setName(user, "Leonardo Melo");
 
 	expect(res).toInclude({firstName: "Leonardo", lastName: "Melo"});
-})
+});
