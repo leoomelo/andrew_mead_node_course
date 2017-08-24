2eac778e950e0a2cb63d8289da1de2e53a83ad61
Leonardo Melo <leoomelo@gmail.com>
2017-08-24 20:10:29 -0300

    implementando o User model

--- section7/node-todo-api/server/server.js
+++ section7/node-todo-api/server/server.js
@@ -3,26 +3,48 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-    type: Boolean
-  },
-  completedAt: {
-    type: Number
+//TODO MODEL
+// var Todo = mongoose.model('Todo', {
+//   text: {
+//     type: String
+//   },
+//   completed: {
+//     type: Boolean
+//   },
+//   completedAt: {
+//     type: Number
+//   }
+// });
+//
+// var newTodo = new Todo({
+//   text: 'Primeiro registro com o mongoose',
+//   completed: false,
+//   completedAt: 34
+// });
+//
+// newTodo.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Não foi possível salvar em Todo');
+// });
+
+//USER MODEL
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    minlength: 1
   }
 });
 
-var newTodo = new Todo({
-  text: 'Primeiro registro com o mongoose',
-  completed: false,
-  completedAt: 34
+var newUser = new User({
+  email: 'leoomelo@hotmail.com'
 });
 
-newTodo.save().then((doc) => {
-  console.log('Saved todo', doc);
+newUser.save().then( (docs) => {
+  console.log('Usuário Salvo', docs);
 }, (e) => {
-  console.log('Não foi possível salvar em Todo');
+  console.log(e);
+  console.log('Não foi possível salvar o usuário');
 });
